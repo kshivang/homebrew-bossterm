ee10177d21317090b6cbe157e9354d94abcb73bf
Risa Labs <enterprise@risalabs.ai>
2026-08-24 07:45:33 +0000

    Update BossTerm to 1.2.153

--- Casks/bossterm.rb
+++ Casks/bossterm.rb
@@ -1,6 +1,6 @@
 cask "bossterm" do
-  version "1.2.152"
-  sha256 "64b8119c40bab151fc2020fdc77c7d9092fb23c3067228e2ced342a5840bac75"
+  version "1.2.153"
+  sha256 "40415409a48f4e6ab5e71f7aae7a0f28b5b57aa7458c8d7b7a3341a844e3653f"
 
   url "https://github.com/kshivang/BossTerm/releases/download/v#{version}/BossTerm-#{version}.dmg",
       verified: "github.com/kshivang/BossTerm/"
